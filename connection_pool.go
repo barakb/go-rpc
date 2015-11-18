686e5d7a7c824bd9d3c0068e050998d882682daa
Barak Bar Orion <barak.bar@gmail.com>
2015-11-18 20:26:14 +0200

    Add ConnectionPool.Close()

--- connection_pool.go
+++ connection_pool.go
@@ -1,11 +1,15 @@
 package rpc
-import "sync"
+import (
+	"sync"
+	"errors"
+)
 
 type ConnectionPool struct {
 	size           int
 	mutex          sync.RWMutex
 	connectionsMap map[string]chan *Connection
-	logger Logger
+	logger         Logger
+	closed         bool
 }
 
 func NewConnectionPool(size int, logger Logger) *ConnectionPool {
@@ -13,25 +17,37 @@ func NewConnectionPool(size int, logger Logger) *ConnectionPool {
 }
 
 func (p *ConnectionPool) Get(address string) (*Connection, error) {
+	p.mutex.Lock();
+	if p.closed {
+		p.mutex.Unlock()
+		return nil, errors.New("Pool is closed")
+	}
 	connections := p.connectionsMap[address]
 	if connections == nil {
-		p.mutex.Lock();
 		if connections = p.connectionsMap[address]; connections == nil {
 			connections = make(chan *Connection, p.size)
 			p.connectionsMap[address] = connections
 		}
-		p.mutex.Unlock()
 	}
+	p.mutex.Unlock()
+
 	select {
 	case res := <-connections:
 		return res, nil
 	default:
+		p.logger.Debug("Creating new connection [%s]", address)
 		return OpenConnection(address)
 	}
 }
 
 func (p *ConnectionPool) Put(connection *Connection) {
 	p.mutex.RLock()
+	if p.closed {
+		p.mutex.Unlock()
+		p.logger.Debug("Disaposing live connection [%s]", connection.address)
+		connection.Close()
+		return
+	}
 	connections := p.connectionsMap[connection.address]
 	p.mutex.RUnlock()
 
@@ -39,8 +55,25 @@ func (p *ConnectionPool) Put(connection *Connection) {
 	case connections <- connection:
 		return
 	default:
+		p.logger.Debug("Disaposing live connection [%s]", connection.address)
 		connection.Close()
 	}
+}
+
+func (p *ConnectionPool) Close() {
+	p.mutex.Lock();
+	p.closed = true
+	p.mutex.Unlock()
+	for _, connections := range p.connectionsMap{
+		close(connections)
+		select {
+		case connection := <-connections:
+			connection.Close()
+		default:
+		}
+	}
+	p.connectionsMap = make(map[string]chan *Connection)
+
 }
 
 
